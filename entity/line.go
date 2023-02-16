890e4e6ac172b0ada1c5f23d512ef092d050897e
cezar <njroffic@gmail.com>
2023-02-16 18:30:58 +0200

    main algorith

--- entity/line.go
+++ entity/line.go
@@ -3,6 +3,10 @@ package line
 import (
 	"fmt"
 	"strings"
+
+	"github.com/cezarovici/goLayouter/app/helpers"
+	"github.com/cezarovici/goLayouter/app/stack"
+	"github.com/cezarovici/goLayouter/domain"
 )
 
 type Line struct {
@@ -36,8 +40,28 @@ func NewLines(content []string) (Lines, error) {
 	return res, nil
 }
 
-func (lines Lines) ParseTo() {
-	//for index, line := range lines {
+func (lines Lines) ParseTo() []domain.Item {
+	var items []domain.Item
 
-	//}
+	var stackPaths stack.Stack
+	var stackIndents stack.Stack
+	//var stackPackages stack.Stack
+
+	for index, line := range lines {
+		if index == 0 {
+			if helpers.TypeOfFile(line.info) == "path" {
+				stackPaths.Push(line.info)
+				stackIndents.Push(-1)
+
+				// I am pushing -1 to set the next element
+				// to be the child of this path
+			}
+
+			stackPaths.Push(line.info)
+			stackIndents.Push(line.level)
+		}
+
+	}
+
+	return items
 }
