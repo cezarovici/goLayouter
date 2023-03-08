17779bc57b5720e420a14aee0f5c6a859d9be148
Apetroaei Cezar <njroffic@gmail.com>
2023-03-08 16:28:22 +0200

    single file for multiple constants

---
+++ app/services/renders/constants.go
@@ -0,0 +1,18 @@
+package renders
+
+const (
+	_testCases = "../templates/"
+
+	_input  = "input/"
+	_output = "output/"
+
+	_mainOutput   = _testCases + _output + "main_result"
+	_objectOutput = _testCases + _output + "object_result"
+	_testOutput   = _testCases + _output + "test_result"
+	_tddOutput    = _testCases + _output + "tableDriven_result"
+
+	_mainIntput  = _testCases + _input + "main.gotmpl"
+	_objectInput = _testCases + _input + "object.gotmpl"
+	_testInput   = _testCases + _input + "test.gotmpl"
+	_tddInput    = _testCases + _input + "tableDriven.gotmpl"
+)
